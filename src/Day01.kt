c7342ff2c04bb387530dab9b519650f8ce59636d
Nico Krijnen <nico.krijnen@luminis.eu>
2024-12-01 12:10:07 +0100

    Solution for day1 part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,16 @@
+import kotlin.math.abs
+
+fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> = input.map {
+    it.split("   ")
+        .let { it[0].toInt() to it[1].toInt() }
+}.unzip()
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val (left, right) = parseInput(input)
+        val sortedInput = left.sorted().zip(right.sorted())
+        val distances = sortedInput.map { abs(it.second - it.first) }
+        return distances.sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -8,11 +18,11 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+//    check(part1(listOf("test_input")) == 6)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
