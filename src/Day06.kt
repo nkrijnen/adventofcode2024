434eb34c91134aec026474a66b043d547ea83869
Nico Krijnen <nico.krijnen@luminis.eu>
2024-12-07 11:48:26 +0100

    Day 6 part 2 solution, but slow (~3 mins)

--- src/Day06.kt
+++ src/Day06.kt
@@ -2,17 +2,17 @@ import Direction.*
 
 fun main() {
     fun part1(input: List<String>): Int {
-        println("grid size: ${input[0].length} x ${input.size}")
         val room = Room(input)
         return room.countVisitedPositions()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val room = Room(input)
+        return room.nrOfPositionForObstructionToGetGuardStuck()
     }
 
     val testInput = readInput("Day06_test")
-    check(Room(testInput).guardStartingPosition == Position(4, 6))
+    check(findStart(testInput) == Position(4, 6))
 
     check(Room(testInput).contains(Position(4, 6)))
     check(!Room(testInput).contains(Position(-1, 0)))
@@ -23,44 +23,62 @@ fun main() {
     check(!Room(testInput).hasObstacleAt(Position(3, 0)))
 
     check(part1(testInput) == 41)
-//    check(part2(testInput) == ?)
+    check(part2(testInput) == 6)
 
     val input = readInput("Day06")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
 
-class Room(private val input: List<String>) {
-    val guardStartingPosition: Position = findStart()
-
-    fun countVisitedPositions(): Int = guardPath().distinct().count()
-
-    fun contains(p: Position): Boolean = p.x >= 0 && p.y >= 0 && p.x < input.size && p.y < input.size
-
-    fun hasObstacleAt(position: Position): Boolean = input[position.y][position.x] == '#'
-
-    private fun findStart(): Position {
-        for (y in input.indices) {
-            for (x in input[y].indices) {
-                if (input[y][x] == '^') return Position(x, y)
-            }
+private fun findStart(input: List<String>): Position {
+    for (y in input.indices) {
+        for (x in input[y].indices) {
+            if (input[y][x] == '^') return Position(x, y)
         }
-        throw IllegalArgumentException("Can't find starting position")
     }
-
-    private fun guardPath(): List<Position> {
-        return Guard(this).pathUntilLeavesRoom()
-    }
+    throw IllegalArgumentException("Can't find starting position")
 }
 
-class Guard(private val room: Room) {
-    private var position = room.guardStartingPosition
+class Room(
+    private val input: List<String>,
+    private val guardStartingPosition: Position = findStart(input),
+    private val obstructionAt: Position? = null
+) {
+    fun countVisitedPositions(): Int = guardPath()!!.distinct().count()
+
+    fun nrOfPositionForObstructionToGetGuardStuck(): Int {
+        return possibleObstructionLocations().count { obstruction ->
+            Room(input, guardStartingPosition, obstruction).doesGuardGetsStuck()
+        }
+    }
+
+    internal fun contains(p: Position): Boolean = p.x >= 0 && p.y >= 0 && p.x < input.size && p.y < input.size
+
+    internal fun hasObstacleAt(position: Position): Boolean =
+        input[position.y][position.x] == '#' || hasObstructionAt(position)
+
+    private fun hasObstructionAt(position: Position): Boolean = when {
+        obstructionAt != null -> obstructionAt == position
+        else -> false
+    }
+
+    private fun guardPath(): List<Position>? {
+        return Guard(this, guardStartingPosition).pathUntilLeavesRoom()
+    }
+
+    private fun doesGuardGetsStuck(): Boolean = guardPath() == null
+
+    private fun possibleObstructionLocations(): List<Position> = input.flatMapIndexed { y, line ->
+        line.mapIndexed { x, c -> if (c == '.') Position(x, y) else null }
+    }.filterNotNull()
+}
+
+class Guard(private val room: Room, private var position: Position) {
     private var direction = N
 
-    fun pathUntilLeavesRoom(): List<Position> {
+    fun pathUntilLeavesRoom(): List<Position>? {
         val path = mutableListOf(position)
         while (true) {
-            println(this)
             val nextPosition = position.movedIn(direction)
             if (!room.contains(nextPosition)) {
                 break
@@ -68,6 +86,10 @@ class Guard(private val room: Room) {
             if (room.hasObstacleAt(nextPosition)) {
                 direction = direction.rotate90Clockwise()
             } else {
+                if (path.isLooping(position, nextPosition)) {
+                    println("looping")
+                    return null
+                }
                 position = nextPosition
                 path.add(position)
             }
@@ -75,7 +97,18 @@ class Guard(private val room: Room) {
         return path.toList()
     }
 
-    override fun toString() = "${position.x},${position.y} -> ${direction}"
+    override fun toString() = "${position.x},${position.y} -> $direction"
+}
+
+// get stuck detection = check if pos & next (on pos change) already appear as pair in path history
+private fun List<Position>.isLooping(position: Position, nextPosition: Position): Boolean {
+    var index = size
+    while (--index > 0) {
+        if (this[index] == nextPosition && this[index - 1] == position) {
+            return true
+        }
+    }
+    return false
 }
 
 enum class Direction {
